9f5c0a7123f396fc98334bab7687b2a6fcb13e53
YuliaGV <74123271+YuliaGV@users.noreply.github.com>
2021-11-11 17:16:50 +0000

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -25,11 +25,11 @@ const Header = ({ toggle }) => {
         <NavBtn>
           <a
             className="btn PrimaryBtn"
-            href="https://drive.google.com/file/d/1iZY6IYfsPXlyKHwGbKcQ-oeBcH7qdwDN/view?usp=sharing"
+            href="https://www.canva.com/design/DAEmdBtlRwA/xgWv-0nt2O5ll8MNjw-W9Q/view?utm_content=DAEmdBtlRwA&utm_campaign=designshare&utm_medium=link&utm_source=sharebutton"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Descargar CV
+            Ver CV
           </a>
         </NavBtn>
         <Bars onClick={toggle} />
